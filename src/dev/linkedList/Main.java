2d4838a5a9181f516624512f7ef480e35dbf07af
fayob <adedejifavour6@gmail.com>
2024-02-20 12:24:38 +0100

    Add append method to linked list

--- src/dev/linkedList/Main.java
+++ src/dev/linkedList/Main.java
@@ -3,10 +3,13 @@ package dev.linkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList linkedList = new LinkedList(4);
+        linkedList.append(7);
+        linkedList.append(6);
+        linkedList.append(5);
 
         linkedList.printList();
-        linkedList.getHead();
-        linkedList.getTail();
-        linkedList.getLength();
+//        linkedList.getHead();
+//        linkedList.getTail();
+//        linkedList.getLength();
     }
 }
