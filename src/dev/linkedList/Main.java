67fb730c7c87bdcb1f2aba71e99be070b640beae
fayob <adedejifavour6@gmail.com>
2024-02-20 12:36:52 +0100

    Add linked list set method

--- src/dev/linkedList/Main.java
+++ src/dev/linkedList/Main.java
@@ -21,6 +21,9 @@ public class Main {
         linkedList.append(4);
         System.out.println("The value at index 1 => " + linkedList.get(1).getValue());
         System.out.println("The value at index 4 => " + linkedList.get(4));
+        System.out.println("Add value at index 2 => " + linkedList.set(2, 6));
+        System.out.println("Add value at index 7 => " + linkedList.set(7, 7));
+        System.out.println("Add value at index 4 => " + linkedList.set(4, 8));
 
         linkedList.printList();
 //        linkedList.getHead();
