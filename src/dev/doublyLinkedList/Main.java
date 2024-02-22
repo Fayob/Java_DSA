77c59e1f48676544678344044627691ae46a4489
fayob <adedejifavour6@gmail.com>
2024-02-22 09:39:25 +0100

    Add Doubly Linked List constructor

---
+++ src/dev/doublyLinkedList/Main.java
@@ -0,0 +1,9 @@
+package dev.doublyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList myDLL = new DoublyLinkedList(2);
+
+        myDLL.getLength();
+    }
+}
