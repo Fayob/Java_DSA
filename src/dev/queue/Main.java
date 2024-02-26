737545d575bcf8997d27e0f662938e1e8e911b3b
fayob <adedejifavour6@gmail.com>
2024-02-26 17:47:59 +0100

    Add Queue constructor

---
+++ src/dev/queue/Main.java
@@ -0,0 +1,12 @@
+package dev.queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(1);
+
+        myQueue.getFirst();
+        myQueue.getLast();
+        myQueue.getLength();
+
+    }
+}
---
+++ src/dev/queue/Queue.java
@@ -0,0 +1,35 @@
+package dev.queue;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        this.first = newNode;
+        this.last = newNode;
+        length = 1;
+    }
+
+    public void getFirst() {
+        System.out.println("First: " + first.value);
+    }
+
+    public void getLast() {
+        System.out.println("Last: " + last.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+}
