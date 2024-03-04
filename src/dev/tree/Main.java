642d0be32a9ec288bc3972a427a3eaf23ee71297
fayob <adedejifavour6@gmail.com>
2024-03-04 17:01:41 +0100

    Add insert method

--- src/dev/tree/Main.java
+++ src/dev/tree/Main.java
@@ -4,6 +4,15 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
 
+        System.out.println(binarySearchTree.insert(47));
+        binarySearchTree.insert(21);
+        binarySearchTree.insert(76);
+        binarySearchTree.insert(18);
+        binarySearchTree.insert(52);
+        binarySearchTree.insert(82);
+
+        binarySearchTree.insert(27);
+
         System.out.println("Root = " + binarySearchTree.root.value);
         System.out.println("Inserted Node = " + binarySearchTree.root.left.right.value);
     }
