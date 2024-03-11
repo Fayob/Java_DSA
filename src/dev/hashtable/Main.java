0dfeb5e19e582d208b06d206d6bc361c072f2292
fayob <adedejifavour6@gmail.com>
2024-03-11 17:21:11 +0100

    Add set method

--- src/dev/hashtable/Main.java
+++ src/dev/hashtable/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         HashTable myHashTable = new HashTable();
 
-        myHashTable.printTable();
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 400);
 
+        myHashTable.printTable();
     }
 }
